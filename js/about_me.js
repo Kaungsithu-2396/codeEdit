9b89fb327b092806d02f88e3746047bdf942eae0
Kaungsithu-123 <Kaungsithu2396@gmail.com>
2023-08-02 14:10:10 +0630

    changes to opacity

--- js/about_me.js
+++ js/about_me.js
@@ -11,13 +11,13 @@ const js = document.getElementById("js");
 const js_text = document.getElementById("js-txt");
 const containerEl = document.querySelectorAll(".skill-meter img"); //so that no duplication occur
 const skillText = document.querySelectorAll(".skill-text p");
-console.log(skillText);
 
 containerEl.forEach((el, idx) => {
     el.addEventListener("mouseover", (e) => {
         el.classList.add("scaleIcon");
         if (skillText[idx].id === el.id + "-txt") {
-            skillText[idx].style.diplay = "block";
+            skillText[idx].style.opacity = 100;
+           
         }
     });
 });
@@ -25,16 +25,8 @@ containerEl.forEach((el, idx) => {
     el.addEventListener("mouseleave", () => {
         el.classList.remove("scaleIcon");
         if (skillText[idx].id === el.id + "-txt") {
-            skillText[idx].style.display = "none";
+            skillText[idx].style.opacity = 0;
+           
         }
     });
 });
-
-var changeclass = function () {
-    ai_text.classList.toggle("hide");
-};
-
-window.onload = function () {
-    var firstDiv = document.getElementsById("ai-text")[0];
-    firstDiv.addEventListener("mouseover", changeclass, false);
-};
